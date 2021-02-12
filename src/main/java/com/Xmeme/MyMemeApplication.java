75d20b5f190d590fac46013626eceb0780861492
Anmol Mittal <anmol15554@gmail.com>
2021-02-13 01:13:52 +0530

    swagger-ui

--- src/main/java/com/Xmeme/MyMemeApplication.java
+++ src/main/java/com/Xmeme/MyMemeApplication.java
@@ -3,7 +3,10 @@ package com.Xmeme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MyMemeApplication {
 
 	public static void main(String[] args) {
